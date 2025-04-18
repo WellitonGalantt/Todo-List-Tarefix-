6a8888f3073be8b87b870cf1973c801e7f763069
WellitonGalantt <devcode.welliton@gmail.com>
2025-04-18 09:51:25 -0300

    fix-create-task-fun

--- backend/src/middlewares/Middlewares.ts
+++ backend/src/middlewares/Middlewares.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import * as yup from 'yup';
 import { IUserRegister } from '../shared/types/userInterfaces';
+import { StatusCodes } from 'http-status-codes';
+import { IReturnData } from '../shared/types/interfaces';
 
 export class Middlewares {
 
     static validateSchema(schema: yup.ObjectSchema<any>) {
-        return async (req: Request, res: Response, next: NextFunction) => {
+        return async (req: Request, res: Response<IReturnData>, next: NextFunction) => {
             try{
                 await schema.validate(req.body, {abortEarly: false});
                 next();
@@ -13,8 +15,11 @@ export class Middlewares {
             }catch(error) {
                 const yupErrors = error as yup.ValidationError;
                 res.status(400).json({
-                    message: 'Erro ao validar o schema',
-                    error: yupErrors.errors
+                    sucess: false,
+                    data: [],
+                    status: StatusCodes.BAD_REQUEST,
+                    message: "Erro ao criar a tarefa!",
+                    error: [yupErrors.errors]
                 })
                 return;
             }
--- frontend/src/js/scripts.js
+++ frontend/src/js/scripts.js
@@ -1,11 +1,39 @@
 //http://localhost:3333
 
+let statusList = [];
+let categoryList = [];
+
 document.addEventListener('DOMContentLoaded', () => {
-    const taskContainerEl = document.querySelector('.task-list')
-    const cretaeTaksBtnEl = document.querySelector('.create-task-button')
-    const cancelEditBtnEl = document.querySelector('.cancel-button')
-    const createTaskModalEl = document.querySelector('#createTaskModal')
-    const createTaskFormEl = document.querySelector('.createTaskForm')
+    const taskContainerEl = document.querySelector('.task-list');
+    const cretaeTaksBtnEl = document.querySelector('.create-task-button');
+    const cancelEditBtnEl = document.querySelector('.cancel-button');
+    const createTaskModalEl = document.querySelector('#createTaskModal');
+    const createTaskFormEl = document.querySelector('#createTaskForm');
+
+    const categorySelectListEl = document.querySelector('#taskCategory');
+    const statusSelectListEl = document.querySelector('#taskStatus');
+
+    async function loadAllData() {
+        try {
+            const statusRes = await fetch("http://localhost:3333/status");
+            if(!statusRes.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+            statusList = await statusRes.json();
+
+            const categoryRes = await fetch("http://localhost:3333/category");
+            if(!categoryRes.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+                
+            categoryList = await categoryRes.json();
+
+            getAllTasks();
+        }
+        catch(err) {
+            console.log("Errro ao carregar os dados " + err)
+        }
+    }
 
 
     function getAllTasks() {
@@ -26,55 +54,255 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    if(cretaeTaksBtnEl){
-        cretaeTaksBtnEl.addEventListener('click', () => {
-            createTaskModalEl.style.display = "block"
-        })
-    }
-
-    if(cancelEditBtnEl){
-        cancelEditBtnEl.addEventListener('click', () => {
-            createTaskModalEl.style.display = "none"
-        })
-    }
-    
-    if (createTaskFormEl) {
-        createTaskFormEl.addEventListener('submit', (event) => {
-            event.preventDefault(); // Impede o envio padrão do formulário
-            const title = document.getElementById('taskTitle').value;
-            const description = document.getElementById('taskDescription').value;
-            const category = document.getElementById('taskCategory').value;
-            const status = document.getElementById('taskStatus').value;
-
-            // Envie esses dados para o seu backend usando fetch (como nos exemplos anteriores)
-            console.log('Dados da nova tarefa:', { title, description, category, status });
-
-            fetch
-
-            // Após salvar, você pode fechar o modal e recarregar a lista de tarefas
-            createTaskModalEl.style.display = "none";
-            // carregarTarefas(); // Se você tiver essa função para atualizar a lista
-        });
-    }
-
     function listAllTask(tasks){
+        taskContainerEl.innerHTML = '';
         tasks[0].forEach(task => {
-            const taskItem = document.createElement('li')
-            taskItem.dataset.taskId = task.id
-            taskItem.classList.add('task-item')
+            const status = getTaskStatus(task.status_id)
+            const category = getTaskCategory(task.category_id)
+            const taskItem = document.createElement('li');
+            taskItem.dataset.taskId = task.id;
+            taskItem.classList.add('task-item');
             taskItem.innerHTML = `
                 <span class="task-title">${task.title}</span>
-                <span class="task-status pendente">Pendente</span>
+                <span class="task-categoria" data-category-id="${category.id}">${category.name}</span>
+                <span class="task-status ${status.name === 'Completa' ? 'concluida' : 'pendente' }" data-status-id="${status.id}">${status.name}</span>
                 <div class="task-actions-buttons">
                     <button class="concluir-button">Concluir</button>
                     <button class="editar-button">Editar</button>
                     <button class="excluir-button">Excluir</button>
                 </div>
             `
-            taskContainerEl.appendChild(taskItem)
+            taskContainerEl.appendChild(taskItem);
+        })
+
+        // const allTasks = document.querySelectorAll('.task-item')
+        taskContainerEl.addEventListener('click', (e) =>{
+            const classElClicked = e.target.classList.value
+            const taskBoxElement = (e.target.parentNode).parentNode
+            const idElClicked = taskBoxElement.dataset.taskId
+
+            if(classElClicked.includes('concluir-button')){
+                completeTask(idElClicked)
+            }
+
+            if(classElClicked.includes('editar-button')){
+                updateDataOptions();
+                if (createTaskFormEl) {
+                    editTask(idElClicked);
+                }
+                
+            }
+
+            if(classElClicked.includes('excluir-button')){
+                fetch(`http://localhost:3333/task/${idElClicked}`, {method: "DELETE"})
+                .then(res => {
+                    if(!res.ok){
+                        throw new Error("Erro na requisicao: " + res.status)
+                    }
+                    getAllTasks();
+                    return res.json();
+                })
+                .then(data => {
+                    console.log(data.message)
+                })
+                .catch(error => {
+                    console.error("Erro ao deletar a tarefa:", error);
+                });
+            }                
+
         })
     }
 
 
-    getAllTasks()
+    if(cretaeTaksBtnEl){
+        cretaeTaksBtnEl.addEventListener('click', () => {
+            updateDataOptions();
+            createTaskModalEl.style.display = "block"
+            if (createTaskFormEl) {
+                createTaskFormEl.addEventListener('submit', createTaskFun);
+            }
+        })
+    }
+
+
+    if(cancelEditBtnEl){
+        cancelEditBtnEl.addEventListener('click', () => {
+            createTaskFormEl.reset();
+            createTaskModalEl.style.display = "none";
+            
+        })
+    }
+
+    //Criacao de tarefas
+    function createTaskFun(event){
+        event.preventDefault();
+        const title = document.getElementById('taskTitle').value.trim();
+        const description = document.getElementById('taskDescription').value.trim();
+        const categorySelectIndex = document.getElementById('taskCategory').selectedIndex;
+        const statusSelectIndex = document.getElementById('taskStatus').selectedIndex;
+        let category_id;
+        let status_id;
+        // --------------------------
+        if( categorySelectIndex !== -1){
+            category_id = document.getElementById('taskCategory').options[categorySelectIndex].dataset.categoryId;
+        }
+
+        if( statusSelectIndex !== -1){
+            status_id = document.getElementById('taskStatus').options[statusSelectIndex].dataset.statusId;
+        }
+
+        const taskData = { title, description,status_id, category_id };
+
+        fetch("http://localhost:3333/task", {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json' // Indica que o corpo da requisição é JSON
+            },
+            body: JSON.stringify(taskData)
+        })
+        .then(res => {
+            if (!res.ok) {
+                console.error(`Erro na requisição: ${res.status}`);
+            }
+            getAllTasks();
+            return res.json();
+        })
+        .then(data => {
+            if(!data.sucess){
+                console.error(`${data.message}\nerror: ${data.error[0]}`)
+            }else{                    
+                createTaskModalEl.style.display = "none";
+                createTaskFormEl.reset();
+                // console.log(data);
+            }
+            
+        })
+        .catch(error => {
+            console.error('Erro ao criar tarefa:', error);
+        });
+
+        createTaskFormEl.removeEventListener('submit', createTaskFun);
+    }
+
+    
+    async function editTask(id){
+        createTaskModalEl.style.display = "block";
+        const task = await getOneTask(id);
+        // console.log(task);
+        const titleInput = document.getElementById("taskTitle");
+        const descInput = document.getElementById("taskDescription");
+        const categorySelect = document.getElementById("taskCategory").getElementsByTagName('option');
+        const statusSelect = document.getElementById("taskStatus").getElementsByTagName('option');
+
+        titleInput.value = task.title;
+        descInput.value = task.description
+        
+        if(categorySelect.length > 0){
+            for(i=0;i<categorySelect.length; i++){
+                if(categorySelect[i].dataset.categoryId == task.category_id){
+                    categorySelect[i].selected = true
+                }
+            }
+        }
+        if(statusSelect.length > 0){
+            for(i=0;i<statusSelect.length; i++){
+                if(statusSelect[i].dataset.statusId == task.status_id){
+                    statusSelect[i].selected = true
+                }
+            }
+        }
+
+        createTaskFormEl.addEventListener('submit', editTaskFun);
+        
+        // const title = elemento.getElementsByClassName("task-title")[0].innerText
+        // const idCategory = elemento.getElementsByClassName("task-categoria")[0].dataset.categoryId
+        // const idStatus = elemento.getElementsByClassName("task-status")[0].dataset.statusId
+        // console.log(idStatus)
+        
+    }
+
+    function editTaskFun(event){
+        event.preventDefault();
+        console.log('Editar!')
+        createTaskFormEl.removeEventListener('submit', editTaskFun);
+        createTaskModalEl.style.display = "none";
+        createTaskFormEl.reset();
+    }
+
+
+    function completeTask(id){
+        fetch(`http://localhost:3333/task/${id}/status`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({status_id: 1})
+        })
+        .then(res => {
+            if (!res.ok){
+                console.error(`Erro na requisição: ${res.status}`);
+            }
+            loadAllData();
+            return res.json();
+        })
+        .then(data => {
+            // console.log(data.message)
+        })
+        
+    }
+
+    function getTaskStatus(id) {
+        return statusList.data[0].find(e => {
+            const idStatus = Number(e.id)
+            return idStatus === id
+        })
+    }
+
+    function getTaskCategory(id) {
+        return categoryList.data[0].find(e => {
+            const idCategory = Number(e.id)
+            return idCategory === id
+        })
+    }
+
+    async function getOneTask(id){
+        return await fetch(`http://localhost:3333/task/${id}`)
+        .then(res => {
+            if(!res.ok){
+                console.warn(`Erro na requisicao: ${res.status}`)
+            }
+            return res.json();
+        })
+        .then(data =>{
+            // console.info(data.message)
+            return data.data[0]
+        })
+        .catch(err =>{
+            console.error(err)
+        })
+    }
+
+    function updateDataOptions() {
+        statusSelectListEl.innerHTML = ''
+        categorySelectListEl.innerHTML = ''
+
+        statusList.data[0].forEach(e => {
+            const option = document.createElement('option')
+            option.value = e.name.toLowerCase()
+            option.text = e.name
+            option.dataset.statusId = e.id
+            statusSelectListEl.appendChild(option)
+        })
+
+        categoryList.data[0].forEach(e => {
+            const option = document.createElement('option')
+            option.value = e.name.toLowerCase()
+            option.text = e.name
+            option.dataset.categoryId = e.id
+            categorySelectListEl.appendChild(option)
+        })
+
+    }
+
+    loadAllData()
 })
